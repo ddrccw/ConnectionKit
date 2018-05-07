c94862e61c034b27f2354cfeda161f8d49b7695c
ddrccw <ddrccw@gmail.com>
2018-05-07 23:00:55 +0800

    MOD: update README.md

--- Package.swift
+++ Package.swift
@@ -10,6 +10,22 @@ import PackageDescription
 
 let package = Package(
     name: "ConnectionKit",
-    dependencies: [],
+    products: [
+        .library(name: "ConnectionKit", targets: ["ConnectionKit-iOS"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Hearst-DD/ObjectMapper.git", majorVersion: 2, minor: 2),
+        .package(url: "https://github.com/krzysztofzablocki/Strongify.git", majorVersion: 1),
+
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", "4.0.0" ..< "5.0.0")
+
+        .package(url: "https://github.com/apple/example-package-fisheryates.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/example-package-playingcard.git", from: "3.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "ConnectionKit-iOS",
+            dependencies: ["ObjectMapper", "Strongify", "RxSwift", "RxCocoa"])
+    ]
     exclude: ["Tests"]
 )
